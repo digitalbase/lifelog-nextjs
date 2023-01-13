6aa0ecb627d48ad3783c936fc59f66d48532c2cd
Gijs Nelissen <gijs@prezly.com>
2023-01-13 15:09:24 +0100

    Get rid of sentry stuff + update theme-kit

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
 });
-const { withSentryConfig } = require('@sentry/nextjs');
 const path = require('path');
 
 const globalSassImports = `\
@@ -101,25 +100,4 @@ const moduleExports = withBundleAnalyzer({
     },
 });
 
-const sentryWebpackPluginOptions = {
-    // Additional config options for the Sentry Webpack plugin. Keep in mind that
-    // the following options are set automatically, and overriding them is not
-    // recommended:
-    //   release, url, org, project, authToken, configFile, stripPrefix,
-    //   urlPrefix, include, ignore
-    // silent: true, // Suppresses all logs
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options.
-};
-
-const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
-
-// TODO: Remove `process.env.VERCEL !== '1'` part when Sentry/Vercel errors are fixed
-const IS_SENTRY_ENABLED =
-    process.env.NODE_ENV === 'production' && process.env.VERCEL !== '1' && SENTRY_DSN;
-
-// Make sure adding Sentry options is the last code to run before exporting, to
-// ensure that your source maps include changes from all other Webpack plugins
-module.exports = IS_SENTRY_ENABLED
-    ? withSentryConfig(moduleExports, sentryWebpackPluginOptions)
-    : moduleExports;
+module.exports = moduleExports;
