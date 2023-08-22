f4571096d3089b2c4e1f308f238bc454e4c2ad3a
Gijs Nelissen <gijs@prezly.com>
2023-08-22 10:00:58 +0200

    Remove line-clamp (now default in 3.3)

--- tailwind.config.js
+++ tailwind.config.js
@@ -305,5 +305,5 @@ module.exports = {
             },
         }),
     },
-    plugins: [require('@tailwindcss/line-clamp'), require('@tailwindcss/typography')],
+    plugins: [require('@tailwindcss/typography')],
 };
