011ed7790a596f19a2b737c01d25960c5e583517
Gijs Nelissen <gijs@prezly.com>
2023-12-02 19:26:54 +0100

    Drop tracking from attachments

--- components/ContentRenderer/components/Attachment/Attachment.tsx
+++ components/ContentRenderer/components/Attachment/Attachment.tsx
@@ -1,6 +1,3 @@
-'use client';
-
-import { STORY_FILE, useAnalytics } from '@prezly/analytics-nextjs';
 import type { AttachmentNode } from '@prezly/story-content-format';
 import { UploadcareFile } from '@prezly/uploadcare';
 
@@ -14,23 +11,18 @@ interface Props {
 }
 
 export function Attachment({ node }: Props) {
-    const { track } = useAnalytics();
     const { file, description } = node;
     const { downloadUrl } = UploadcareFile.createFromPrezlyStoragePayload(file);
     const displayedName = description || file.filename;
     const fileExtension = file.filename.split('.').pop();
     const fileType = fileExtension?.toUpperCase();
 
-    function handleClick() {
-        track(STORY_FILE.DOWNLOAD, { id: file.uuid });
-    }
 
     return (
         <a
             id={`attachment-${file.uuid}`}
             className={styles.container}
             href={downloadUrl}
-            onClick={handleClick}
         >
             <div className={styles.icon}></div>
             <div className={styles.content}>
