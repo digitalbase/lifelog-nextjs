5449aabd3d46e62205b0aff2b6750f0c56e28064
Gijs Nelissen <gijs@prezly.com>
2023-12-02 14:52:24 +0100

    Add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,38 @@
+import { Sitemap } from '@prezly/theme-kit-nextjs/server';
+import type { MetadataRoute } from 'next';
+
+import { app } from '@/lib/adapters/app';
+
+const MINUTE = 60;
+
+export const revalidate = 15 * MINUTE;
+
+function generateUrl(type = string, object) {
+    if (!object) return;
+    if (!object.slug) return;
+
+    switch (type) {
+        case 'category':
+            return `/category/${object?.slug}`
+        case 'story':
+            return `/${object?.slug}`
+    }
+    return '/';
+}
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+
+    return Sitemap.generate(
+        {
+            generateUrl,
+            categories: app().categories,
+            newsroom: app().newsroom,
+            locales: app().locales,
+            stories: app().allStories,
+        },
+        {
+            baseUrl: process.env.NEXT_PUBLIC_URL,
+        },
+    );
+}
+
