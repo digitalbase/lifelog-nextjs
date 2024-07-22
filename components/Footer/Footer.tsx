b02295216fe6cf4407a5441ec48f6276bb6853aa
digitalbase <gijs@prezly.com>
2024-07-22 22:33:15 +0200

    Remove changelog from header and footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -41,7 +41,6 @@ export function Footer() {
                                     How I built this blog
                                 </NavLink>
                                 <NavLink href="/uses">Uses</NavLink>
-                                <NavLink href="/changelog">Changelog</NavLink>
                                 <NavLink href="https://www.twitter.com/digitalbase">
                                     Contact
                                 </NavLink>
